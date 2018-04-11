55d70bd5e9dd1adcbf19f1b7f274debda70127d5
Luis Delgado <ledodo99@hotmial.com>
2018-04-11 08:43:33 -0600

    Tabajo

---
+++ Lab2/Assets/Scripts/BirdScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    public float jumpForce = 200f;
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        if (!GameController.instance.gameOver)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * jumpForce);
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameController.instance.score++;
+        int score = GameController.instance.score;
+        Debug.Log(score);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameController.instance.gameOver = true;
+    }
+
+
+}
